cc198dc6e8c6c67a8acd2b6891010a8d83f481e6
Rennan <rennankelson@gmail.com>
2023-07-28 17:37:11 -0300

    anotação esqueci no commit anterior

--- backend/src/main/java/com/pessoas/cadastro/repository/LoginRepository.java
+++ backend/src/main/java/com/pessoas/cadastro/repository/LoginRepository.java
@@ -2,7 +2,9 @@ package com.pessoas.cadastro.repository;
 
 import com.pessoas.cadastro.entity.Login;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
 
+@Repository
 public interface LoginRepository extends JpaRepository <Login, Long>{
     Login findByUsuarioAndSenha(String usuario, String senha);
 }
